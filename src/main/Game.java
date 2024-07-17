7114f49d51a919efbc587467ae3b1a6c55062e4d
Aakif Kohari <kohariaakif+github@gmail.com>
2024-07-18 00:32:05 +0530

    Added Game Loop (& Set an FPS Limit)
    Stopped Annoying MouseListener Behaviour
    
    Added a Game Infinite Loop which updates the panel
    also prints the FPS to Console
    
    Added Methods in GamePanel that move in both x and y direction until border
    also added random color generator to make color change everytime the rectangle hits border
    
    Now Rectangle Moves Like a Screensaver
    
    Step #03

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,60 @@
 package main;
 
-public class Game 
+import java.awt.Toolkit;
+
+public class Game implements Runnable
 {
-	
 	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+	private Thread gameThread;
+	private final int FPS_SET = 120;
+	static int HEIGHT;
+	static int WIDTH;
+	
 	public Game() 
 	{
-		GamePanel gamePanel = new GamePanel();
+		gamePanel = new GamePanel();
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
+		HEIGHT = gameWindow.frame.getContentPane().getSize().height;
+		WIDTH = gameWindow.frame.getContentPane().getSize().width;
+		startGameLoop();
+	}
+	
+	private void startGameLoop()
+	{
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() 
+	{
+		double timePerFrame = 1000000000.0 / FPS_SET;
+		long lastFrame = System.nanoTime();
+		long now = System.nanoTime();
+		
+		int frames = 0;
+		long lastCheck = System.currentTimeMillis();
+		
+		while(true)
+		{
+			if(System.getProperty("os.name").equals("Linux"))
+				Toolkit.getDefaultToolkit().sync();	// Linux Lags Fixed  
+			now = System.nanoTime();
+			if(now - lastFrame >= timePerFrame)
+			{
+				gamePanel.repaint();
+				lastFrame = now;
+				frames++;
+			}
+			
+			if(System.currentTimeMillis() - lastCheck >= 1000)
+			{
+				lastCheck = System.currentTimeMillis();
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
 	}
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,18 +1,26 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 import javax.swing.JPanel;
-
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
 public class GamePanel extends JPanel
 {
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100;
+	private float xDelta = 100, yDelta = 100;
+	private float xDir = 1, yDir = 1;
+	private int frames = 0;
+	private long lastCheck = 0;
+	private Color color = new Color(150, 20, 90);
+	private Random random;
+
 	
 	public GamePanel() 
 	{
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
@@ -22,23 +30,47 @@ public class GamePanel extends JPanel
 	public void addXDelta(int value)
 	{
 		this.xDelta += value;
-		repaint();
 	}
 	public void addYDelta(int value)
 	{
 		this.yDelta += value;
-		repaint();
 	}
 	public void setRectPosition(int x, int y)
 	{
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 	public void paintComponent(Graphics g) 
 	{
 		super.paintComponent(g);
 		
-		g.fillRect(xDelta, yDelta, 200, 50);
+		updateRect();
+		g.setColor(color);
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+	}
+
+	private void updateRect() 
+	{
+		xDelta += xDir;
+		if((xDelta + 200) > Game.WIDTH || xDelta < 0)
+		{			
+			xDir *= -1;
+			color = getRndmColor();
+		}
+		yDelta += yDir;
+		if((yDelta + 50) > Game.HEIGHT - 3 || yDelta < 0)	//for some reason it has 3 extra pixels below bottom
+		{			
+			yDir *= -1;
+			color = getRndmColor();
+		}
+	}
+
+	private Color getRndmColor() 
+	{
+		int r = random.nextInt(255);
+		int g = random.nextInt(255);
+		int b = random.nextInt(255);
+		
+		return new Color(r,g,b);
 	}
 }
