284320183e90edd375ba8e6b6d6b806734045ec3
Aakif Kohari <kohariaakif+github@gmail.com>
2024-07-18 00:32:05 +0530

    Added Game Loop (& Set an FPS Limit)
    Stopped Annoying MouseListener Behaviour
    
    Added a Game Infinite Loop which updates the panel
    also prints the FPS to Console
    
    Added Methods in GamePanel that move in both x and y direction until border
    also added random color generator to make color change everytime the rectangle hits border
    
    Now Rectangle Moves Like a Screensaver

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -8,9 +8,9 @@ public class MainClass
 		//This is main class of the Adventure Ball Game 
 		//This is where the Game will be initiated from
 		
-		
-		System.setProperty("sun.java2d.opengl", "true");
-		//This is a property I added makes animations smoother
+//		if(System.getProperty("os.name").equals("Linux"))
+//			System.setProperty("sun.java2d.opengl", "true");
+		//This is a property I added makes animations smoother in Linux but for some reason it makes the game crash will further test
 		
 		new Game();
 	}
